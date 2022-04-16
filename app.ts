ebbbd1f59c5a269a6cc40a9752c2ad90bb4ddd37
Harold Torres Marino <haroldtm55@gmail.com>
2022-04-17 09:19:44 +1000

    Array types

--- app.ts
+++ app.ts
@@ -1,16 +1,26 @@
 // Sub-optimal
+
 // const person: {
 //   name: string
 //   age: number
 // } = {
 //   name: 'Harold',
-//   age: 30
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking']
 // }
 
 // Optimal
 const person = {
   name: 'Harold',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
 
+let favouriteActivities: string[]
+favouriteActivities = ['Sports']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
